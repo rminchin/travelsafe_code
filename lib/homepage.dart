f80de31c6ccfe9eb296d463b22233762f6e6b054
Rhys Minchin <rhysminchin@hotmail.co.uk>
2022-04-03 13:51:08 +0100

    *added navbar to homepage with 5 tabs for various incoming functionality *created settings.dart and moved logout feature there *added option to toggle auto login on/off in settings page

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,13 +1,8 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:travelsafe_v1/helpers/user.dart';
 
-import 'package:shared_preferences/shared_preferences.dart';
-
 import 'emergency.dart';
-import 'loginSignUp.dart';
-import 'package:travelsafe_v1/helpers/database_helper.dart';
+import 'settings.dart';
 
 class HomePage extends StatefulWidget {
   final User user;
@@ -17,22 +12,9 @@ class HomePage extends StatefulWidget {
   HomePageState createState() => HomePageState();
 }
 
-class HomePageState extends State<HomePage> {
-  bool _alertValue = false;
-  SharedPreferences?
-      _preferences; //using shared preferences for 'staying logged in'
-
-  @override
-  void initState() {
-    super.initState();
-    initializePreference().whenComplete((){
-      setState(() {});
-    });
-  }
-
-  Future<void> initializePreference() async {
-    _preferences = await SharedPreferences.getInstance();
-  }
+class HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  int _currentIndex = 2;
 
   void _submitEmergency() {
     Navigator.push<void>(
@@ -41,93 +23,88 @@ class HomePageState extends State<HomePage> {
             builder: (BuildContext context) => Emergency(user: widget.user)));
   }
 
-  _submitLogoutAlert(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text("Log out"),
-          content: StatefulBuilder(
-              builder: (BuildContext context, StateSetter setState) {
-            return ListTile(
-                title: const Text("Tick this box if you no longer wish to automatically log in when opening the app"),
-                trailing: Checkbox(
-                  value: _alertValue,
-                  onChanged: (value) {
-                    setState(() {
-                      _alertValue = value!;
-                    });
-                  },
-                ),
-              );
-          }),
-          actions: [
-            TextButton(
-              child: const Text("Log Out"),
-              onPressed: () {
-                _alertValue ? _submitLogout('yes') : _submitLogout('');
-              },
-            )
-          ],
-          elevation: 5,
-        );
-      },
-    );
-  }
-
-  _submitLogout(String toDo) async {
-    if (toDo != '') {
-      await _preferences?.remove('username');
-      await DatabaseHelper.updateUser(widget.user.username, widget.user.password, widget.user.nickname, false);
-    }
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Successfully logged out'),
-    ));
-    Navigator.push<void>(
-        context,
-        MaterialPageRoute<void>(
-            builder: (BuildContext context) => const LoginSignUp()));
-  }
-
   @override
   Widget build(BuildContext context) {
+    Widget widget2 = Container(); // default
+    switch (_currentIndex) {
+      case 0:
+        widget2 = const FlutterLogo(
+          size: 100,
+        );
+        break;
+
+      case 1:
+        widget2 = const FlutterLogo(
+          size: 200,
+        );
+        break;
+
+      case 2:
+        widget2 = Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Logged in as ${widget.user.nickname}'),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: _submitEmergency,
+                  child: Text(
+                    'Emergency',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.redAccent),
+                  ),
+                ),
+              ]),
+        );
+        break;
+
+      case 3:
+        widget2 = const FlutterLogo(
+          size: 300,
+        );
+        break;
+
+      case 4:
+        widget2 = Settings(user: widget.user);
+        break;
+    }
     return WillPopScope(
       onWillPop: () async => false,
-      child: Scaffold(
+      child: DefaultTabController(
+        length: 5,
+        child: Scaffold(
           extendBody: true,
           appBar: AppBar(
-              automaticallyImplyLeading: false, title: const Text("Homepage")),
-          body: Center(
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text('Logged in as ${widget.user.nickname}'),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: _submitEmergency,
-                    child: Text(
-                      'Emergency',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.redAccent),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  ElevatedButton(
-                    onPressed: () =>
-                    _preferences?.getString('username') != null
-                        ? _submitLogoutAlert(context)
-                        : _submitLogout(''),
-                    child: Text(
-                      'Log Out',
-                      style: Theme.of(context).textTheme.headline6,
-                    ),
-                  )
-                ]),
-          )),
+              automaticallyImplyLeading: false, title: const Text("TravelSafe")),
+          body: widget2,
+          bottomNavigationBar: BottomNavigationBar(
+              type: BottomNavigationBarType.fixed,
+              selectedItemColor: Colors.blue[700],
+              selectedFontSize: 15,
+              unselectedFontSize: 13,
+              iconSize: 30,
+              currentIndex: _currentIndex,
+              onTap: (newIndex) => setState(() {
+                    _currentIndex = newIndex;
+                  }),
+              items: const [
+                BottomNavigationBarItem(
+                    label: "Map", icon: Icon(Icons.location_on_sharp)),
+                BottomNavigationBarItem(
+                    label: "Network", icon: Icon(Icons.people_outline_rounded)),
+                BottomNavigationBarItem(
+                    label: "Home", icon: Icon(Icons.home_rounded)),
+                BottomNavigationBarItem(
+                    label: "Chat", icon: Icon(Icons.question_answer_rounded)),
+                BottomNavigationBarItem(
+                    label: "Settings", icon: Icon(Icons.settings)),
+              ]),
+        ),
+      ),
     );
   }
 }
---
+++ lib/settings.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:travelsafe_v1/helpers/user.dart';
+import 'loginSignUp.dart';
+import 'package:travelsafe_v1/helpers/database_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Settings extends StatefulWidget {
+  final User user;
+  const Settings(
+      {Key? key, required this.user})
+      : super(key: key);
+
+  @override
+  SettingsState createState() => SettingsState();
+}
+
+class SettingsState extends State<Settings> {
+  late bool _value;
+  SharedPreferences?
+  _preferences; //using shared preferences for 'staying logged in'
+
+  @override
+  void initState() {
+    super.initState();
+    _value = widget.user.autoLogin == 1;
+    initializePreference().whenComplete(() {
+      setState(() {});
+    });
+  }
+
+  Future<void> initializePreference() async {
+    _preferences = await SharedPreferences.getInstance();
+  }
+
+  _submitLogout() async {
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Successfully logged out'),
+    ));
+    Navigator.push<void>(
+        context,
+        MaterialPageRoute<void>(
+            builder: (BuildContext context) => const LoginSignUp()));
+  }
+
+  updateUserAutoLogin() async {
+    await DatabaseHelper.updateUser(widget.user.username, widget.user.password, widget.user.nickname, _value);
+    if(_value){
+      _preferences?.setString('username', widget.user.username);
+      print("autologin is now true");
+    } else{
+      _preferences?.remove('username');
+      print("autologin is now false");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+          child: Column(
+              children: [
+                Text('Settings - logged in as ${widget.user.nickname}'),
+                const SizedBox(height: 30),
+                ListTile(
+                  title: const Text("Auto log in feature:"),
+                  trailing: Checkbox(
+                    value: _value,
+                    onChanged: (value) {
+                      setState(() {
+                        _value = value!;
+                        updateUserAutoLogin();
+                      });
+                    },
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () => _submitLogout(),
+                  child: Text(
+                    'Log Out',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                )
+              ]
+          ),
+        )
+    );
+  }
+}
